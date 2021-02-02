9ec970846138ef6bfa059a9d81a770e19c2cf687
Luccas Santos <luccasjsantos@hotmail.com>
2021-02-01 21:01:35 -0300

    :lipstick: No more decimal places when formatting a number less than 1000

--- util.js
+++ util.js
@@ -2,7 +2,8 @@ import numeral from 'numeral'
 
 export const percentFormat = x => `${parseFloat(x * 100).toFixed(1)}%`
 
-export const numberAbrevFormat = x => numeral(x).format('0.0a').toUpperCase()
+export const numberAbrevFormat = x =>
+  x < 1000 ? x : numeral(x).format('0.0a').toUpperCase()
 
 export const numberFormat = x => numeral(x).format('0,0')
 
